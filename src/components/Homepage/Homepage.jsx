4a815489f3c55c82f13734e9aeb7d4e7592149bf
Zack Tillotson <ZTillotson@aetna.com>
2019-02-23 10:09:17 -0700

    Mostly finished with v1 homepage. Needs images & editing

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -10,56 +10,92 @@ import './styles';
 const Homepage = function({children}) {
   return (
     <Fragment>
-      <header>
-        <div className="content">
+      <header className="homepage__header">
+        <div className="column__one">
           <h1>
-            <span className="preline">Get a job with</span>
-            Elemental Interviewing
+            Elemental
           </h1>
-          <p className="bragging">This is a proven strategy with step by step guidelines. I'll break down the Elements of getting a great job.</p>
+          <p className="bragging">A proven step by step guide to a successful job hunt. Interviewing is a game - learn the rules, practice the skills, & get a job.</p>
+        </div>
+        <div className="column__two">
+          <figure role="presentation">
+            <img className="nav-link__logo" src="https://res.cloudinary.com/indysigner/image/upload/v1540550092/SmashingConf/sf19-cat-circle.svg" />
+          </figure>
         </div>
       </header>
       <section className="content content--two-column">
         <div className="column__two">
-          <h2>Companies want to hire you!</h2>
-          <p>Finding an employee is expensive. The employer is spending money, time, and energy to look through resumes, conduct phone screens, and do interviews. The further through the process a candidate is the more money is wasted when they pass on that candidate. They'll only make money after they hire someone. This means they are incentized to find someone!</p>
+          <h2>Companies want to hire someone</h2>
+          <p>Finding an employee is expensive. The employer is spending money, time, and energy to look through resumes, conduct phone screens, and do interviews. The further through the process a candidate is the more money is wasted when they pass - they'll only make money after they hire someone. This means they are incentivized to find someone.</p>
+          <p>For employers it comes down to three simple questions, once they find someone who answers them correctly they will hire that person.</p>
         </div>
       </section>
-      <section className="content content--mini-one-column">
-        <h2 className="content-headline">Answer these 3 questions to get hired</h2>
+      <section className="content content--mini-one-column highlight">
+        <h2 className="content-headline">The 3 Elemental questions to get hired</h2>
         <h3 className="ordered-list--important">1. Can you do the job?</h3>
         <p>Are you technical capable of doing this job? Are you experienced enough? Can you learn the aspects of the job you don't already know?</p>
         <h3 className="ordered-list--important">2. Will you do the job?</h3>
-        <p>Are you motivated and professional? Will you show up on time? Are you going to fall asleep during the day or read Facebook instead of working?</p>
+        <p>Are you motivated and professional? Will you show up on time? Are you going to take a nap or surf the web instead of working?</p>
         <h3 className="ordered-list--important">3. Are you annoying?</h3>
         <p>Are we going to get along? How do you handle stress? Are you high maintinance? Do our personallities clash?</p>
-        <h2 className="extra-spaced">All other questions boil down to these Elemental questions</h2>
+      </section>
+      <section className="content">
+        <h2>Everything reduces to these 3 Elemental questions</h2>
+        <p>For example, they'll look at your Résumé for the industry "buzzwords" that show you'll be able to do the job - answering question #1 "Can you do the job?".</p>
+        <p>Another example, what does it say if you are a few minutes late for an interview? Maybe don't actually care about this job? Answering question #2 "Will you do the job?"</p>
+        <p>Next we'll go over each step of the job hunt process.</p>
       </section>
       <section className="secondary-dark">
         <div className="content">
-          <h2 className="content-headline">What should you expect during the hiring process?</h2>
+          <h2 className="content-headline">The steps of the hiring process</h2>
           <ol className="process-overview content content--mini-one-column">
             <li>
               <figure role="presentation" dangerouslySetInnerHTML={{__html: svgResume}} />
               <h3>1. Create a Résumé</h3>
-              <p>A Résumé is two things. First it's <span className="bold">how you land interviews</span>. Second it then drives <span className="bold">what you talk about</span> during the interview</p>
-              <Link to="/resume/">Learn more</Link>
+              <p>A Résumé is two things.</p>
+              <ol>
+                <li>
+                  <div className="bold">1. How you land interviews</div>
+                  Managers will look at your résumé for (literally) 5 seconds, it needs to tell them something in that time.
+                </li>
+                <li>
+                  <div className="bold">2. Drive the conversation</div>
+                  An interviewer will look over your resume for about 60 seconds. They'll ask you to give more detail about what they see, so your résumé should contain the things you want to talk about.
+                </li>
+              </ol>
+              <Link to="/resume/">Learn more →</Link>
             </li>
             <li>
               <figure role="presentation" dangerouslySetInnerHTML={{__html: svgPhone}} />
               <h3>2. Go through a phone screen</h3>
-              <p>The phone screen is a <span className="bold">short technical interview</span>. They're hard to prepare for, but a little research goes a long ways.</p>
+              <p>It's a short technical interview. They're hard to prepare for, but a little research goes a long ways.</p>
+              <Link to="/phone-screen/">Learn more →</Link>
             </li>
             <li>
               <figure role="presentation" dangerouslySetInnerHTML={{__html: svgPerson}} />
-              <h3>3. Conduct Interviews</h3>
-              <p>The interview is the most stressful, most exciting part of the whole process.</p>
-              <Link to="/interview/">Learn more></Link>
+              <h3>3. Interview</h3>
+              <p>Interviewing can be broken down in to 3 parts.</p>
+              <ol>
+                <li>
+                  <div className="bold">1. Tell me about yourself</div>
+                  i.e. "Why should we hire you? This is by far the most common first question and also the most often answered improperly.
+                </li>
+                <li>
+                  <div className="bold">2. All other questions</div>
+                  Regardless of the specific question, focus on answering the 3 Elemental questions. I use a technique called "Stories" which makes answering any question a breeze.
+                </li>
+                <li>
+                  <div className="bold">3. Your questions for them</div>
+                  This is your chance to put an exclamation point on the interview.
+                </li>
+              </ol>
+              <Link to="/interview/">Learn more →</Link>
             </li>
             <li>
               <figure role="presentation" dangerouslySetInnerHTML={{__html: svgHandshake}} />
               <h3>4. Sometimes you need to follow up</h3>
               <p>It can be awkward, but it's often the difference between sitting on someone's "to do" list and getting the offer.</p>
+              <Link to="/follow-up/">Learn more →</Link>
             </li>
           </ol>
         </div>
