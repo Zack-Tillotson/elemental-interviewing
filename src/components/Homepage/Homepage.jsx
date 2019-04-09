1486fbefec35936083bb39483b18b13b976f4a36
Zack Tillotson <ZTillotson@aetna.com>
2019-04-09 16:21:31 -0600

    Changing content to be in box with light grey background. Cleans up homepage content.

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -16,7 +16,7 @@ const Homepage = function({children}) {
             <h1>
               Interviewing Simplified
             </h1>
-            <p className="bragging">Learn the foundation of a successful interview: answering the 3 Elemental questions. Then learn specific step by step tips for creating a résumé, doing the phone screen, full in person interviews, and following up.</p>
+            <p className="bragging">Learn the foundation of a successful interview: answering the 3 Elemental questions.</p>
           </div>
           <div className="column__two">
             <figure role="presentation">
@@ -25,29 +25,29 @@ const Homepage = function({children}) {
           </div>
         </div>
       </header>
-      <section className="highlight">
-        <div className="content">
-          <h2 className="content-headline">There are only 3 elemental questions</h2>
-          <h3 className="ordered-list--important">1. Can you do the job?</h3>
+      <section className="content highlight">
+          <h2 className="content-headline">There are only 3 Elemental questions</h2>
+          <h3 className="ordered-list--important">Can you do the job?</h3>
           <p>Are you technical capable of doing this job? Are you experienced enough? Can you learn the aspects of the job you don't already know?</p>
-          <h3 className="ordered-list--important">2. Will you do the job?</h3>
+          <p>This question is trying to make sure an employee has the experience and technical skills to do a job. Many candidates think this is the only thing they need to show in order to get a job, but it's just the first step!</p>
+          <h3 className="ordered-list--important">Will you do the job?</h3>
           <p>Are you motivated and professional? Will you show up on time? Are you going to take a nap or surf the web instead of working?</p>
-          <h3 className="ordered-list--important">3. Are you annoying?</h3>
+          <p>This question is different from the first, it is asking about your motivation and professionalism. An employer wants to know that the person they're hiring has the right attitude! For example, if a candidate is over qualified for a job they still might not be hired. Why? Because the employer might be afraid that candidate would leave soon after being hired for a better job.</p>
+          <h3 className="ordered-list--important">Are you annoying?</h3>
           <p>Are we going to get along? How do you handle stress? Are you high maintinance? Do our personallities clash?</p>
-        </div>
-      </section>
-      <section className="content content--two-column">
-        <div className="column__two">
-          <h2>Companies want to hire someone</h2>
-          <p>Finding an employee is expensive. The employer is spending money, time, and energy to look through resumes, conduct phone screens, and do interviews. The further through the process a candidate is the more money is wasted when they pass - they'll only make money after they hire someone. This means they are incentivized to find someone.</p>
-          <p>For employers it comes down to three simple questions, once they find someone who answers them correctly they will hire that person.</p>
-        </div>
       </section>
       <section className="content">
-        <h2>All other questions reduce to the 3 Elemental questions</h2>
+        <h2>That's it! Answer those questions and get hired</h2>
+        <p>Finding an employee is expensive. The employer is spending money, time, and energy to look through resumes, conduct phone screens, and do interviews. The further through the process a candidate is the more money is wasted when they pass - they'll only make money after they hire someone. This means they are incentivized to find someone.</p>
+        <p>For employers it comes down to these three elemntal questions, once they find someone who answers them correctly they will hire that person.</p>
+      </section>
+      <section className="content">
+        <h2>Everything in the job hunt boils down to the 3 Elemental questions</h2>
+        <p>Regardless of what question you're asked, what weird interview process you're asked to go through, or how you're clothing is judged, it's all because the employer is seeking to answer the 3 Elemental questions.</p>
         <p>For example, they'll look at your Résumé for the industry "buzzwords" that show you'll be able to do the job - answering question #1 "Can you do the job?".</p>
         <p>Another example, what does it say if you are a few minutes late for an interview? Maybe don't actually care about this job? Answering question #2 "Will you do the job?"</p>
-        <p>Next we'll go over each step of the job hunt process.</p>
+        <p>When trying to get a job the most important thing is answering these 3 Elemental questions. The rest of this site will give specific strategies and tips for your résumé, for phone screening, and for interviewing. You can follow them or not, but do remember the 3 Elemental questions, they are all that matter!</p>
+        <p>Next we'll go over strategies & tips for each step of the job hunt process.</p>
       </section>
       <section className="content process-overview">
         <h3>The stages of the hiring process</h3>
