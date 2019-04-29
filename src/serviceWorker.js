28ad92988e8552e598d884dc907f5b6b9bd286d3
Zack Tillotson <ZTillotson@aetna.com>
2019-04-29 10:25:39 -0600

    Updating service worker to use Workbox

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,10 +1,9 @@
-var CACHE_NAME = 'my-site-cache-v3';
-var urlsToCache = [
-  '/',
-  '/resume/',
-  '/interview/',
-  '/phone-screen/',
-  '/follow-up/',
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js');
+
+import routes from './routes';
+
+const appRoutes = routes.map(route => route.path);
+const appImages = [
   '/assets/comic_200.webp',
   '/assets/resume-180.webp',
   '/assets/phone-180.webp',
@@ -12,64 +11,55 @@ var urlsToCache = [
   '/assets/follow-180.webp',
 ];
 
+// Serving assets /////////////
+
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'fonts-stylesheets',
+  })
+);
+
+workbox.routing.registerRoute(
+  /^https:\/\/www\.googletagmanager\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'analytics',
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'app-images',
+  })
+);
+
+workbox.routing.registerRoute(
+  /\/$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'app-pages',
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.js$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'app-js',
+  })
+);
+
+// Preloading assets ///////////////
+
+
+function preloadApp() {
+  const appPromise = caches.open('app-pages')
+    .then(cache => cache.addAll(appRoutes));
+  const imagePromise = caches.open('app-images')
+    .then(cache => cache.addAll(appImages));
+
+  return Promise.all([appPromise, imagePromise]);
+}
+
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = ['my-site-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+  event.waitUntil(preloadApp());
 });
