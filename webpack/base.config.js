921bf3ea705284fc2734f840c81b3efd62385ace
Zack Tillotson <ZTillotson@aetna.com>
2019-10-08 13:21:49 -0600

    Updates content & title font to Cizel

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -32,9 +32,6 @@ const config = {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        options: {
-          presets: ['es2015'],
-        },
       },
       {
         test: /\.scss$/,
